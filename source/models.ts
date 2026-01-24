f494137433baf1087dd0103514302f4052561016
alejandro gonzalez flores <agf_001@outlook.com>
2026-01-23 23:44:39 -0800

    modificacion1

--- source/models.ts
+++ source/models.ts
@@ -1,12 +1,36 @@
-// este import existe solo para que tsc lo tome y lo copie a /build
-import "./contacts.json";
-// si no estuviera este import typescript no se da cuenta que lo necesitamos
-// ya que para escribir o leer al archivo usamos la libreria "jsonfile"
+import fs from 'fs';
 
 class Contact {
   id: number = 0;
   name: string = "";
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  contacts: Contact[] = [];
+
+  load() {
+    const data = fs.readFileSync('contacts.json', 'utf-8');
+    this.contacts = JSON.parse(data);
+  }
+
+  getAll() {
+    return this.contacts;
+  }
+
+  getOneById(id: number): Contact | null {
+    const contact = this.contacts.find(contact => contact.id === id);
+    return contact || null;
+  }
+
+  addOne(contact: Contact) {
+    this.contacts.push(contact);
+    this.save();
+  }
+
+  save() {
+    const data = JSON.stringify(this.contacts, null, 2);
+    fs.writeFileSync('contacts.json', data, 'utf-8');
+  }
+}
+
 export { ContactsCollection };
